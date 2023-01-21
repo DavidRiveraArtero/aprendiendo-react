4d8691869781a42273ff03e7565dfbcaa54af3f1
DavidRiveraArtero <davidtoy222@gmail.com>
2023-01-21 16:59:47 +0100

    TwitterPostCard

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,40 +1,74 @@
 
 import './App.css'
 import { TwitterCard } from './TwitterFollowCard'
+import { TwitterPostCard } from './TwitterPostCard'
 
 export function App(){
     const users = [
         {
-            userName:    'midudev',
-            name:        'Migue Angel Duran',
-            isFollowing: false
+            userName    : 'midudev',
+            name        : 'Migue Angel Duran',
+            isFollowing : false
         },
         {
-            userName:    'dase',
-            name:        'Dase Zero',
-            isFollowing: true
+            userName    :'dase',
+            name        :'Dase Zero',
+            isFollowing : true
         },
         {
-            userName:    'Pedro',
-            name:        'Pedor Zero',
-            isFollowing: true
+            userName    :'Pedro',
+            name        :'Pedor Zero',
+            isFollowing : true
+        },
+    ]
+
+    const post = [
+        {
+            nameTw   :  'midudev',
+            userName :  'Migue Angel Duran',
+            content  :  'Mi primer video de js',
+            liked    :   false,
+            retw     :   true
+            
+        },
+        {
+            nameTw   :  'evilafm',
+            userName :  'Alexelcapo',
+            content  :  'Nuevo video de DS3',
+            liked    :   true,
+            retw     :   false
         },
     ]
     
     // para pasar variables como boleans, arrays, etc en el elemento tendremos que poner {}
     return(
-      
-        <section className='App'>
-           {
-            users.map(({userName, name, isFollowing}) =>{
-               
-                return(
-                    <TwitterCard key={userName} userName={userName} initialIsFollowing={isFollowing}>
-                        {name}
-                    </TwitterCard>
-                )
-            })
-           }
-        </section>
+        <>
+            <h1 style={{color:"white"}}>Twitter Fan</h1>
+            <section className='twitter_contentPostCard'>
+                {
+                    post.map(({nameTw,content,userName,liked, retw})=>{
+                        return(
+                            <TwitterPostCard key={nameTw} nameTw={nameTw} content={content} liked={liked} retw={retw}>
+                                {userName}
+                            </TwitterPostCard>
+                        )
+                    })
+                }                
+            </section>
+            
+            <section className='twitter_contentFollowCard'>
+
+                {
+                    users.map(({userName, name, isFollowing}) =>{
+                    
+                        return(
+                            <TwitterCard key={userName} userName={userName} initialIsFollowing={isFollowing}>
+                                {name}
+                            </TwitterCard>
+                        )
+                    })
+                }
+            </section>
+        </>
     )
 }
