1a6870132a1c2d9410e41bc8e64e3749dbb7d728
DavidRiveraArtero <davidtoy222@gmail.com>
2023-01-26 14:27:07 +0100

    Tabla Puntuajes

--- projects/01-tres-en-raya/src/App.jsx
+++ projects/01-tres-en-raya/src/App.jsx
@@ -10,6 +10,10 @@ function App() {
   const [turn, setTurn] = useState(TURNS.X)
   const [win, setWin] = useState(null) // null es que no hay ganador, false es empate y true hay ganador
 
+  const [victX, setVictX] = useState(0)
+  const [victO, setVictO] = useState(0)
+
+
   const checkEndGame = (newBoard) =>{
     return newBoard.every((square) => square != null)
   }
@@ -27,11 +31,22 @@ function App() {
     const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
 
-    const newWiner = checkWinerFrom(newBord)
+    const newWiner = checkWinerFrom(newBord,setVictX,setVictO)
     if(newWiner){
+
+      // SUMAR VICTORIAS
+      if(newWiner == 'x'){
+        setVictX(victX + 1)
+      }else{
+        setVictY(victO + 1)
+      }
+
       setWin(newWiner)
+
     }else if(checkEndGame(newBord)){
+
       setWin(false)
+
     }
 
   }
@@ -43,37 +58,45 @@ function App() {
   }
   
   return (
+    <>
+      <main className="board">
+        <h1>Tres en raya </h1>
+        <button onClick={resetGame}>Resetear</button>
+        <section className="game">
+          {
+            board.map((_,index)=>{
+              return(
+                <Square key={index} index={index} updateBoard={updateBoard} turn={turn}>
+                  {board[index]}
+                </Square>
+              )
+            })
+          }
+        </section>
 
-    <main className="board">
-      <h1>Tres en raya </h1>
-      <button onClick={resetGame}>Resetear</button>
-      <section className="game">
-        {
-          board.map((_,index)=>{
-            return(
-              <Square key={index} index={index} updateBoard={updateBoard} turn={turn}>
-                {board[index]}
-              </Square>
-            )
-          })
-        }
+        <section className="turn">
+
+          <Square isSelected={turn === TURNS.X}>
+            {TURNS.X}
+          </Square>
+
+          <Square isSelected={turn === TURNS.O}>
+            {TURNS.O}
+          </Square>
+
+        </section>
+        
+        <Winner win={win} resetGame={resetGame}></Winner>
+      </main>
+
+      <section className="board puntos">
+        <h1>Victorias</h1><br/>
+        <h1>{TURNS.X} = <span> {victX}</span></h1>
+        <h1>{TURNS.O} = <span> {victO}</span></h1>
       </section>
-
-      <section className="turn">
-
-        <Square isSelected={turn === TURNS.X}>
-          {TURNS.X}
-        </Square>
-
-        <Square isSelected={turn === TURNS.O}>
-          {TURNS.O}
-        </Square>
-
-      </section>
-     
-     <Winner win={win} resetGame={resetGame}></Winner>
-
-    </main>
+    </>
+   
+    
 
     
   )
