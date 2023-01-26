a6e411a015e7796a5c564f8599491186c4a19fe5
DavidRiveraArtero <davidtoy222@gmail.com>
2023-01-26 13:57:15 +0100

    logic board

--- projects/01-tres-en-raya/src/logic/board.js
+++ projects/01-tres-en-raya/src/logic/board.js
@@ -4,12 +4,14 @@ export const checkWinerFrom = (boardToCheck) => {
 
     //Revisamos todas las combinaciones ganadoras
     for(const combo of WINER_COMBO){
-      const [a, b, c] = combo
-      if(boardToCheck[a] && // 0 -> X u O
+        const [a, b, c] = combo
+        if(boardToCheck[a] && // 0 -> X u O
         boardToCheck[a] === boardToCheck[b] // Miramos si se repite
         && boardToCheck[a] === boardToCheck[c]){ // Lo mismo
-        return boardToCheck[a]
-      }
+
+            return boardToCheck[a]
+            
+        }
     }
     return null
 }
