04543771b0839837afd23b088f1913fb262aee56
Rivera Artero <driveraa@t-systems.es>
2023-01-31 14:46:57 +0100

    Tablero Figuras

--- projects/02-ajedrez/src/App.jsx
+++ projects/02-ajedrez/src/App.jsx
@@ -3,7 +3,26 @@ import './App.css'
 import { BoardComponent } from './components/board/boardComponent'
 
 function App() {
-  const [board, setBoard] = useState(new Array(64).fill(null,0,8).fill('♙',8,16).fill(null,16,48).fill('♟',48,56).fill(null,56,64))
+  const [board, setBoard] = useState(new Array(64)
+                                      .fill('♖',0,1)
+                                      .fill('♘',1,2)
+                                      .fill('♗',2,3)
+                                      .fill('♕',3,4)
+                                      .fill('♔',4,5)
+                                      .fill('♗',5,6)
+                                      .fill('♘',6,7)
+                                      .fill('♖',7,8)
+                                      .fill('♙',8,16)
+                                      .fill(null,16,48)
+                                      .fill('♟',48,56)
+                                      .fill("♖",56,57)
+                                      .fill("♘",57,58)
+                                      .fill('♗',58,59)
+                                      .fill('♕',59,60)
+                                      .fill('♔',60,61)
+                                      .fill('♗',61,62)
+                                      .fill('♘',62,63)
+                                      .fill('♖',63,64))
   // Blanca ♟ Negra ♟
     
   return (
