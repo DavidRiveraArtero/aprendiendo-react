b2956c4f05ff7f4891552db7b52e761e38775455
DavidRiveraArtero <davidtoy222@gmail.com>
2023-03-05 18:09:34 +0100

    mouse follower

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,16 +1,55 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
-import './App.css'
+
 
 function App() {
 
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
   useEffect(()=>{
-    console.log("Efecto")
-  })
+    const handleMove = (event) =>{
+      const {clientX, clientY} = event
+    
+      setPosition({x:clientX, y: clientY})
+    }
+
+    if(enabled){
+      window.addEventListener('pointermove',handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove',handleMove)
+    } // CUANDO SE RENDERICE DE NUEVO EL USEFFECT ANTES TENEMOS QUE LIMPIAR LAS SUSCRIPCIONES ANTERIORES  
+
+  },[enabled]) // SOLO se ejecutare cuando enabled cambie de estado
 
 
   return (
-    <h3>Mouse Follower</h3>
+    <main>
+  
+      <div style={
+              {
+                position: 'absolute',
+                backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                border: '1px solid #fff',
+                borderRadius: '50%',
+                opacity: 0.8,
+                pointerEvents: 'none',
+                left: -25,
+                top: -25,
+                width: 50,
+                height: 50,
+                transform: `translate(${position.x}px, ${position.y}px)`
+              }}/>
+     
+
+      
+      <button onClick={()=> setEnabled(!enabled)}>
+        {enabled ? 'Desactivar': 'Activar'}
+      </button>
+    </main>
+
   )
 }
 
